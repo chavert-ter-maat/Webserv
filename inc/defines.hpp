5ec5e78502dd68479580c2a8122a5f1110acd76f
Chavert home <chavert@live.nl>
2024-08-01 15:30:34 +0200

    clientConnection refactored & log, serverConnection cleaned. Needs stress testing and leak testing

--- inc/defines.hpp
+++ inc/defines.hpp
@@ -2,16 +2,8 @@
 
 #define MSG_BORDER "---------------------------"
 
-#define BUFFSIZE 4096
-
-#define QUEUESIZE 128
-
 #define CRLF "\r\n"
 
 #define CRLFCRLF "\r\n\r\n"
 
-#define PATH_LOGFILE "./logDir/logfile.log"
 
-#define MAX_LOG_SIZE 100000
-
-#define AMOUNT_LINES_APPEND 100
