ce79aa41b983a1a5083d3b138853a6eaed022361
Martin <mhaan@student.codam.nl>
2024-09-20 14:08:28 +0200

    Added necessary html to bmi.cpp

--- cgi_src/bmi.cpp
+++ cgi_src/bmi.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
 
-float bmi(float weight, float height) { return weight / (height * height); }
+double bmi(double weight, double height) { return weight / (height * height); }
 
 int main(int argc, char **argv) {
+  double weight;
+  double height;
+
+  std::cout << "Content-Type: " << "text/html\r\n\r\n";
+  std::cout << "<!DOCTYPE html><html lang=\"en\"><head><title>BMI "
+               "Calculator</title></head><body>"
+            << std::endl;
+
   if (argc != 3) {
-    std::cerr << "Usage: " << argv[0] << " <weight> <height>" << std::endl;
-    return 1;
+    std::cout << "Program takes two input params: 'weight (in kg)' and 'height "
+                 "(in m)'"
+              << std::endl;
+    std::cout << "</body></html>";
+    return 0;
   }
 
-  float weight = std::stof(argv[1]);
-  float height = std::stof(argv[2]);
-  float result = bmi(weight, height);
+  try {
+    weight = std::stod(argv[1]);
+    height = std::stod(argv[2]);
+  } catch (std::exception &e) {
+    std::cout << "Invalid input" << std::endl;
+    std::cout << "</body></html>";
+    return 0;
+  }
+  double result = bmi(weight, height);
+  std::cout << "Weight = " << weight << " kg" << std::endl;
+  std::cout << "Height = " << height << " m" << std::endl;
   std::cout << "BMI = " << result << std::endl;
-  return 0;
+  std::cout << "</body></html>";
 }
