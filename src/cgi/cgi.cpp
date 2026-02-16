c6854e6dd5b368f6600ffe9e7ed4d878a677cdd6
Chavert ter Maat <chavert@live.nl>
2026-02-16 11:09:22 +0100

    Update cgi.cpp

--- src/cgi/cgi.cpp
+++ src/cgi/cgi.cpp
@@ -243,7 +243,7 @@ int	CGI::writeCGIfd()
 	to_write = _request->get_body().length() - _bytesWritten;
 	if (to_write > BUFFSIZE)
 		to_write = BUFFSIZE;
-	size_t bytes_written = write(_pollfdWrite.fd, _request->get_body().data() + _bytesWritten, to_write);
+	ssize_t bytes_written = write(_pollfdWrite.fd, _request->get_body().data() + _bytesWritten, to_write);
 	_bytesWritten += to_write;
 	if (_bytesWritten == _request->get_body().length())
 	{
